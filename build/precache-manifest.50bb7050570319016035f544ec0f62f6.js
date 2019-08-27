c63c7894a900834eaffb9d9ab59b8a52467a0338
Tyler Hill <t.wakefield.hill@gmail.com>
2019-08-27 09:36:38 -0400

    change folder structure

---
+++ build/precache-manifest.50bb7050570319016035f544ec0f62f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c11cfb0290cd030dd980e266ec55d82",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f0286077d6721a90f3d",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "a4ceff244c7c49d9243e",
+    "url": "/static/css/main.3150ee01.chunk.css"
+  },
+  {
+    "revision": "6f0286077d6721a90f3d",
+    "url": "/static/js/2.ddde166c.chunk.js"
+  },
+  {
+    "revision": "a4ceff244c7c49d9243e",
+    "url": "/static/js/main.625c210f.chunk.js"
+  },
+  {
+    "revision": "b501c6a0a1bee1e0a4f8",
+    "url": "/static/js/runtime~main.b996a010.js"
+  }
+]);
